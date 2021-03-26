77ad7deebbb22f7fcf2a7b73065f88d2274ba3e2
HungHN180091 <hung.hn180091@sis.hust.edu.vn>
2021-03-26 14:41:24 +0700

    upload

---
+++ assignment5/src/states/entity/player/PlayerIdleState.lua
@@ -0,0 +1,38 @@
+--[[
+    GD50
+    Legend of Zelda
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+PlayerIdleState = Class{__includes = EntityIdleState}
+
+function PlayerIdleState:enter(params)
+    -- render offset for spaced character sprite
+    self.entity.offsetY = 5
+    self.entity.offsetX = 0
+end
+
+function PlayerIdleState:update(dt)
+    EntityIdleState.update(self, dt)
+end
+
+function PlayerIdleState:update(dt)
+    if love.keyboard.isDown('left') or love.keyboard.isDown('right') or
+       love.keyboard.isDown('up') or love.keyboard.isDown('down') then
+        self.entity:changeState('walk')
+    end
+
+    if love.keyboard.wasPressed('space') then
+        if not self.entity.pot then
+            self.entity:changeState('swing-sword')
+        else
+            -- change the animation
+            self.entity.pot = false
+
+            -- reload the idle state
+            self.entity:changeState('idle')
+        end
+    end
+end
---
+++ assignment9/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool alive = true;
+    private static int level = 0;
+	public Text levelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // increase number of stage level
+		level++;
+		DisplayLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -20)
+        {
+            alive = false;
+        }
+        GameOver();
+    }
+
+    void GameOver()
+    {
+        if (!alive)
+        {
+            level = 0;
+            Destroy(GameObject.Find("WhisperSource"));
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+    void DisplayLevel()
+	{
+		levelText.text = "Stage: " + level;
+	}
+}
